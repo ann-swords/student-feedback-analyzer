9af2a5bb4d9bf39845d66c5ca4b4f03c24c78378
Anwaar <anwaaralsyouf@gmail.com>
2022-12-07 11:31:25 +0300

    basic charts added

--- feedbackanalyzer/main_app/views.py
+++ feedbackanalyzer/main_app/views.py
@@ -11,19 +11,9 @@ environ.Env.read_env()
 # Create your views here.
 
 # Classes:
-# class deliverableCreate():
-#   model = Cat
-#   fields = ['name', 'breed', 'description', 'age']
-
-
-
-#   analysis=response.body[0]['classifications'][0]['tag_name']
-
-# Classes:
 class DeliverableCreate(CreateView):
   model = Deliverable
   fields = ['units', 'hmwname', 'githubrepo', 'comments', 'date']
-
 
 
 class DeliverableUpdate(UpdateView):
@@ -82,6 +72,11 @@ def deliverable_detail(request, del_id):
 
 # function that analyzes the comments using sentiment analysis.
 def analyzer(request):
+  unit1 = Deliverable.objects.filter(units='1')
+  unit2 = Deliverable.objects.filter(units='2')
+  unit3 = Deliverable.objects.filter(units='3')
+  unit4 = Deliverable.objects.filter(units='4')
+
   ml = MonkeyLearn(env('MONKEY_LEARN_PASS'))
   b_data = Deliverable.objects.latest('id')
   response = ml.classifiers.classify(
@@ -93,4 +88,5 @@ def analyzer(request):
   b_data = b_data.id
   a_data = Deliverable.objects.filter(id=b_data).update(analysis = response.body[0]['classifications'][0]['tag_name'])
   # deliverables = Deliverable.objects.all()
-  return render(request, 'deliverables/analyzer.html', {'analyze': response.body[0]['classifications'][0]['tag_name']})
+  return render(request, 'deliverables/analyzer.html', {'analyze': response.body[0]['classifications'][0]['tag_name'], 'unit1': unit1, 'unit2': unit2, 'unit3': unit3, 'unit4': unit4})
+
